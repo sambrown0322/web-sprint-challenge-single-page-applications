e7b1bcf6a450ef26c01c8d22f73ae36ad433decf
David Brown <davidbrown@Davids-iMac.local>
2020-07-25 14:24:32 -0600

    interview q's

--- cypress/integration/examples/pizzaTests.js
+++ cypress/integration/examples/pizzaTests.js
@@ -1,15 +1,15 @@
 describe("Inputs, toppings, submit", () => {
   it("Can nav to site", () => {
-    cy.visit("http://localhost:3002/pizza");
+    cy.visit("http://localhost:3000/pizza");
     cy.url().should("include", "localhost");
   });
   it("can add text", () => {
     cy.get("#nameInput").type("Sam").should("have.value", "Sam");
   });
   it("can select multiple toppings", () => {
-    cy.get(".toppings > :nth-child(1) > input").click();
     cy.get(".toppings > :nth-child(2) > input").click();
-    cy.get(".toppings > :nth-child(3) > input").click();
+    cy.get(".toppings > :nth-child(4) > input").click();
+    cy.get(".toppings > :nth-child(6) > input").click();
   });
   it("can submit", () => {
     cy.get("button").click();
